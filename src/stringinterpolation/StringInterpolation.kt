fdf3e798af80cccd8bda634d3be0b751c1f8ddb4
Avinash <arai08230@gmail.com>
2020-09-19 14:18:11 +0530

    added string interpolation

--- src/stringinterpolation/StringInterpolation.kt
+++ src/stringinterpolation/StringInterpolation.kt
@@ -1,2 +1,10 @@
 package stringinterpolation
 
+fun main(args: Array<String>){
+    var n1:Int = 5
+    var n2:Int = 10
+    var sum: Int = n1 + n2
+    val char:String = "Kotlin"
+    println("Sum of $n1 and $n2 is $sum")
+    println("$char Basic Programs")
+}
