a5d47d695170849f431aa094f17fe77c2d33d56c
Avinash <arai08230@gmail.com>
2020-09-19 14:16:14 +0530

    added first hello word program

--- src/kotlinfirstprogram/HelloWord.kt
+++ src/kotlinfirstprogram/HelloWord.kt
@@ -1,4 +1,5 @@
 package kotlinfirstprogram
-fun main(args: Array<String>){
-    println("Hello, Word!!")
+fun main(args: Array<String>){ //this is the main method starting point of kotlin program
+    print("Hello, Word!!") //print same line
+    println("Welcome, Kotlin Programming!!..") // print next line
 }
