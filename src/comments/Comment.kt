8e7b551ee7889842dc1c5e205bcaa657d5cfa98e
Avinash <arai08230@gmail.com>
2020-09-19 14:19:01 +0530

    added comments

--- src/comments/Comment.kt
+++ src/comments/Comment.kt
@@ -1,2 +1,10 @@
 package comments
 
+fun main(){
+    //This is single line comment
+    /* This is multi line comment
+       kotlin programming
+     */
+    println("//This is single line comment")
+    println("/* This is a multi line comment \n kotlin basic programming \n */")
+}
