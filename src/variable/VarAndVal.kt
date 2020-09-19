3da3599a0fc1c22d700577d4e4f26e17cb1190a3
Avinash <arai08230@gmail.com>
2020-09-19 14:17:08 +0530

    added variables

--- src/variable/VarAndVal.kt
+++ src/variable/VarAndVal.kt
@@ -1,2 +1,13 @@
 package variable
 
+fun main(args: Array<String>){
+    var temp:Int = 5  //mutable variable change later
+    val number:Int = 6 //Immutable variable doesn't change
+    val a:String  //declare
+    a = "Kotlin" //assign
+    println(temp)
+    println(number)
+    temp = 10
+    println(temp)
+    println(a)
+}
