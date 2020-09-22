82535d5dfa18b79b94e2f47588004790011f8187
Priyanka Khandge <priyakhandge10@gmail.com>
2020-09-22 22:14:45 +0530

    added input output

--- src/inputoutput/Input.kt
+++ src/inputoutput/Input.kt
@@ -1,2 +1,23 @@
 package inputoutput
 
+import java.util.*
+
+fun main(){
+    /*
+    standard library function readLine() which is used for reads line of string input from standard input stream.
+    It returns the line read or null.
+    */
+    println("Enter your name:")
+    val name = readLine()
+    println("Enter your age:")
+    val age:Int = Integer.valueOf(readLine())
+    println("my name is $name and my age is $age")
+    /*
+    While using the readLine() function, input lines other than String are explicitly converted into their corresponding types.
+    To input other data type rather than String, we need to use Scanner object of java.util.Scanner class from Java standard library.
+     */
+    val read = Scanner(System.`in`)
+    println("Enter your age:")
+    val age1: Int = read.nextInt()
+    println("your age is $age1")
+}
--- src/inputoutput/Output.kt
+++ src/inputoutput/Output.kt
@@ -1,2 +1,17 @@
 package inputoutput
-
+/*
+ Kotlin standard input output operations are performed to flow byte stream from input device (keyboard)
+ to main memory and from main memory to output device (screen).
+*/
+fun main(){
+    /*
+    Kotlin output operation is performed using the standard methods print() and println().
+        1. print(): used to print values provided inside the method "()".
+        2. println(): used to print values provided inside the method "()" and
+        moves cursor to the beginning of next line.
+     */
+    println("Hello")
+    println("Welcome to kotlin tutorial")
+    print("say")
+    print("Hi")
+}
